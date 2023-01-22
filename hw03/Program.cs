7ced9bbe5fe637cfcc886ecb2ab433abe3552b88
yulia <yuliagorbunova@yahoo.com>
2023-01-22 21:24:25 +0500

    hw_3

--- hw03/Program.cs
+++ hw03/Program.cs
@@ -4,3 +4,24 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+
+int GetUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+void Kub(int number)
+{
+    int numberK = 1;
+    while (numberK <= number)
+    {
+        int result = (int)Math.Pow(numberK,3);
+        Console.Write(result + " ");
+        numberK++;
+    }
+}
+
+int number = GetUserValue("Введите число");
+Kub(number);
